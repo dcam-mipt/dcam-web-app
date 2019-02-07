33e132c2f8d4f981dd3baf25f405a8073f53d5d2
mityabeldii <mityabeldii@gmail.com>
2019-02-07 23:21:29 +0300

    07.02.2019🌿23:21

---
+++ src/constants/example_config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sabir on 19.02.17.
+ */
+
+export default {
+    PARSE_SERVER_URL: `XXX.XXX.XXX.XX`,
+    PARSE_APP_ID: `your_app_id`,
+    PARSE_JS_KEY: `your_js_key`,
+    PARSE_MASTER_KEY: `your_master_key`,
+}
