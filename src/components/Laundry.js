fb0540ecae8b7e46be0244723f8ca836ae7f26c6
mityabeldii <mityabeldii@gmail.com>
2019-05-09 23:25:10 +0300

    09.05.2019🌿23:25

--- src/components/Laundry.js
+++ src/components/Laundry.js
@@ -289,7 +289,7 @@ background-color: transparent;
 @media (min-width: 320px) and (max-width: 480px) {
     display: ${props => props.mobileCalendar ? `block` : `none`};
     width: 100vw;
-    max-height: 100vh;
+    max-height: ${13 * 14}vw;
     overflow: scroll;
 }`
 
@@ -303,7 +303,7 @@ background-color: transparent;
 @media (min-width: 320px) and (max-width: 480px) {
     display: ${props => props.mobileCalendar ? `none` : `block`};
     width: 100vw;
-    max-height: 100vh;
+    max-height: ${13 * 14}vw;
     overflow: scroll;
 }`
 
