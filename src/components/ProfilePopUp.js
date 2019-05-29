ff95996941330c92a9ef35b0fd8b8036b7707f9d
mityabeldii <mityabeldii@gmail.com>
2019-05-29 04:55:26 +0300

    29.05.2019🌿04:55

--- src/components/ProfilePopUp.js
+++ src/components/ProfilePopUp.js
@@ -1,5 +1,5 @@
 /*eslint-disable no-unused-vars*/
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Flex, Image, Text, Bar, BarWrapper, PopUp } from './styled-templates'
 import moment from 'moment'
 import Button from './Button'
@@ -11,6 +11,8 @@ import Input from './Input'
 import CardPopUp from './CardPopUp';
 import TelegramAuthPopUp from './TelegramAuthPopUp';
 import useComponentVisible from './useComponentVisible'
+import io from 'socket.io-client';
+const socket = io('http://dcam.pro:3000');
 
 let get_ser_status = (timestamp) => {
     if (+moment() - +timestamp < 5 * 6000) {
@@ -23,11 +25,20 @@ let get_ser_status = (timestamp) => {
 }
 
 let main = (props) => {
-    let { user, balance, signOut } = props
+    let { user, signOut } = props
     let [verificationRef, verificationVisible, setVerificationVisible] = useComponentVisible(true);
+    let [entries, setEntries] = useState(false)
+    let [value, setValue] = useState(``)
+    useEffect(() => {
+        socket.on('verifications update', function (msg) {
+            axios.get(`http://dcam.pro/api/auth/get_my_entries`)
+                .then((d) => { setEntries(d.data) })
+                .catch((d) => { console.log(d) })
+        })
+    })
     return <BarWrapper>
-        <PopUp top={3} right={4.5} ref={verificationRef} visible={verificationVisible} >
-            <TelegramAuthPopUp/>
+        <PopUp top={3} right={3} ref={verificationRef} visible={verificationVisible} >
+            <TelegramAuthPopUp />
         </PopUp>
         <Bar row >
             <Image src={require(`../assets/images/home.svg`)} width={2} />
@@ -43,9 +54,32 @@ let main = (props) => {
                 <Text color={mvConsts.colors.text.support} >{user && get_ser_status(user.last_seen)}</Text>
             </NameWrapper>
         </Bar>
-        {/* <Bar>
-            <Text color={mvConsts.colors.text.support} >telegram бот ещё не взаимнодейтсовал с этим аккаунтом</Text>
-        </Bar> */}
+        {
+            user && user.telegram_id
+                ? <Bar row extra={`justify-content: flex-start;`} >
+                    <Text>telegram бот активирован</Text>
+                    {/* <Switch/> */}
+                </Bar>
+                : <Flex>
+                    {
+                        !entries && <Bar extra={`align-items: flex-start;`} >
+                            <Text color={mvConsts.colors.text.support} >telegram бот ещё не активирован. Он будет отправлять вам во</Text>
+                            <Text>Найдите наш бот (@dcam_mipt_bot) в telegram</Text>
+                            <Text>Отправьте ему команду /auth</Text>
+                        </Bar>
+                    }
+                    {
+                        entries && <Bar row >
+                            <Input placeholder={`*****`} number type={`password`} short onChange={(d) => { !isNaN(d.target.value) && setValue(d.target.value) }} />
+                            <Button backgroundColor={mvConsts.colors.accept} onClick={() => {
+                                axios.get(`http://dcam.pro/api/auth/accept_verificatoin_pass/${value}`)
+                                    .then((d) => { console.log(d) })
+                                    .catch((d) => { console.log(d) })
+                            }} >Подтвердить</Button>
+                        </Bar>
+                    }
+                </Flex>
+        }
         <Bar only_mobile >
             <CardPopUp />
         </Bar>
