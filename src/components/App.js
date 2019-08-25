8adbab1aae70c75176da7c20b2f3380272483733
mityabeldii <mityabeldii@gmail.com>
2019-08-25 14:16:17 +0300

    25.08.2019🌿14:16

--- src/components/App.js
+++ src/components/App.js
@@ -1,64 +1,31 @@
 /*eslint-disable no-unused-vars*/
 import React, { useState, useEffect } from 'react'
-import GoogleAPI from '../API/GoogleAPI'
-import styled from 'styled-components'
-import { connect } from 'react-redux'
-import Main from './Main'
-import Entry from './Entry'
 import { Flex, Image, Text, Rotor } from './UIKit/styled-templates'
+import Button from './UIKit/Button'
 import mvConsts from '../constants/mvConsts'
-import moment from 'moment'
 
-let theme = mvConsts.colors
-
-let GoogleWrapper = (props) => {
-    let [init, setInit] = useState(false)
+let App = () => {
     useEffect(() => {
-        GoogleAPI.init()
-            .then((d) => { setInit(true) })
-            .catch((d) => {
-                console.log(`google initialization error`, d)
-            })
-    })
-    return init ? props.user.token ? <Main /> : <Entry /> : <LoadingPage />
-    // return <Sorry/>
-}
-
-let Sorry = () => {
+        setTimeout(() => {
+            window.location.replace(`http://dcam.pro`)
+        }, 2000)
+    }, [])
     return (
-        <Flex extra={`width: 100vw; height: 100vh; background-color: ${mvConsts.colors.purple}`} >
-            {/* <Text size={2} >😞</Text> */}
-            <Text color={`white`} size={1.2} > Извините, сайт временно находится на обслуживании, зайдите утречком ещё раз.</Text>
-            <Text color={`white`} bold extra={`margin-top: 2vh;`} >{moment().startOf(`day`).format(`DD.MM.YY`)}</Text>
+        <Flex extra={`width: 100vw; height: 100vh; background: ${mvConsts.colors.purple};`} >
+            <Image src={require(`../assets/images/move.svg`)} width={15} />
+            <Text bold color={`white`} size={1.5} extra={`margin-bottom: 3vw; @media (min-width: 320px) and (max-width: 480px) { margin-bottom: 15vw; }`} >Мы переехали</Text>
+            <Button
+                backgroundColor={mvConsts.colors.accept}
+                extra={`&:hover { transform: rotate(2deg) scale(1.05) };`}
+                onClick={() => {
+                    window.location.replace(`http://dcam.pro`)
+                }}
+            >
+                dcam.pro
+            </Button>
         </Flex>
     )
 }
 
-let LoadingPage = (props) => {
-    return (
-        <LoadingPageWrapper>
-            <Flex>
-                <Rotor><Image src={require(`../assets/images/menu.svg`)} width={5} /></Rotor>
-                <Text>Подключаем почту</Text>
-            </Flex>
-        </LoadingPageWrapper>
-    )
-}
-
-const LoadingPageWrapper = styled(Flex)`
-width: 100vw;
-height: 100vh;
-background-color: ${props => props.background.primary};
-@media (min-width: 320px) and (max-width: 480px) {
-    
-}`
-
-let mapStateToProps = (state) => {
-    return {
-        user: state.user
-    }
-}
-
-connect(mapStateToProps)(LoadingPage)
-export default connect(mapStateToProps)(GoogleWrapper)
+export default App;
 /*eslint-enable no-unused-vars*/
--- src/index.js
+++ src/index.js
@@ -4,22 +4,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App.js'
 import * as serviceWorker from './serviceWorker';
-import * as Sentry from '@sentry/browser';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import { reducer } from './redux/reducers'
-
-const store = createStore(reducer);
-
-// Sentry.init({
-//     dsn: "https://11f6f8cd95ef4b56882cfaafedcc3b59@sentry.io/1417450"
-// });
 
 let main = () => {
     return (
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <App />
     )
 }
 
